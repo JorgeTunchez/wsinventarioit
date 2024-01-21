274178a2062cf4a6ea848d9b84c189a3fdf1edd4
JorgeTunchez <jorge.tunchez@gmail.com>
2024-01-20 20:47:08 -0600

    cambios en estructura y nuevas funciones en el modelo

--- model/wsinventarioitmodel.php
+++ model/wsinventarioitmodel.php
@@ -1,10 +1,79 @@
 <?php 
-
 include("core/core.php");
 
-function obtenerData($id){
+
+function obtenerHardware($expediente){
     $arrDatos = array();
-    $sql = "SELECT CONCAT(colaborador.nombres,' ',colaborador.apellidos) nombre_completo,
+    $sql = "  SELECT componente.nombre nombrecomponente, 
+                     expediente_detail.marca, 
+                     CASE WHEN expediente_detail.valor='-' THEN '' ELSE expediente_detail.valor END valor,
+                     expediente_detail.serie,
+                     expediente_detail.modelo,
+                     expediente_detail.linea,
+                     expediente_detail.anio  
+                FROM expediente_detail 
+                     INNER JOIN componente ON expediente_detail.componente = componente.id
+                     INNER JOIN categoria ON componente.categoria = categoria.id
+               WHERE expediente_detail.expediente = $expediente
+                 AND categoria.id = 1";
+    $result = executeQuery($sql);
+    if (!empty($result)) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $arrDatos[$row["nombrecomponente"]]["MARCA"] = $row["marca"];
+            $arrDatos[$row["nombrecomponente"]]["VALOR"] = $row["valor"];
+            $arrDatos[$row["nombrecomponente"]]["SERIE"] = $row["serie"];
+            $arrDatos[$row["nombrecomponente"]]["MODELO"] = $row["modelo"];
+            $arrDatos[$row["nombrecomponente"]]["LINEA"] = $row["linea"];
+            $arrDatos[$row["nombrecomponente"]]["AÑO"] = $row["anio"];
+        }
+    }
+    return $arrDatos;
+}
+
+
+function obtenerSoftware($expediente){
+    $arrDatos = array();
+    $sql = "SELECT TRIM(componente.nombre) software, 
+                   CASE WHEN expediente_detail.pago = 1 THEN 'Si' ELSE 'No' END pago
+              FROM expediente_detail 
+                   INNER JOIN componente ON expediente_detail.componente = componente.id
+                   INNER JOIN categoria ON componente.categoria = categoria.id
+             WHERE expediente_detail.expediente = $expediente
+               AND categoria.id = 2
+          ORDER BY componente.nombre";
+    $result = executeQuery($sql);
+    if (!empty($result)) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $arrDatos[$row["software"]]["PAGO"] = $row["pago"];
+        }
+    }
+    return $arrDatos;
+}
+
+
+function obtenerPlataformas($expediente){
+    $arrDatos = array();
+    $sql = "SELECT TRIM(componente.nombre) plataforma, 
+                   CASE WHEN expediente_detail.pago = 1 THEN 'Si' ELSE 'No' END pago
+              FROM expediente_detail 
+                   INNER JOIN componente ON expediente_detail.componente = componente.id
+                   INNER JOIN categoria ON componente.categoria = categoria.id
+             WHERE expediente_detail.expediente = $expediente
+               AND categoria.id = 5
+          ORDER BY componente.nombre";
+    $result = executeQuery($sql);
+    if (!empty($result)) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $arrDatos[$row["plataforma"]]["PAGO"] = $row["pago"];
+        }
+    }
+    return $arrDatos;
+}
+
+
+function obtenerData($cif){
+    $arrDatos = array();
+    $sql = "SELECT CONCAT(TRIM(colaborador.nombres),' ',TRIM(colaborador.apellidos)) nombre_completo,
                    colaborador.cif,
                    colaborador.puesto,
                    area.nombre area,
@@ -12,23 +81,27 @@ function obtenerData($id){
                    expedientes.direccionip,
                    expedientes.dominio,
                    expedientes.usuario,
-                   CASE WHEN colaborador.activo = 1 THEN 'Si' ELSE 'No' END activo
+                   CASE WHEN colaborador.activo = 1 THEN 'Si' ELSE 'No' END activo,
+                   expedientes.id expediente
               FROM colaborador
                    INNER JOIN area ON colaborador.area = area.id
                    INNER JOIN expedientes ON expedientes.colaborador = colaborador.id
-             WHERE colaborador.id = $id";
+             WHERE colaborador.cif = $cif";
     $result = executeQuery($sql);
     if (!empty($result)) {
         while ($row = mysqli_fetch_assoc($result)) {
-            $arrDatos["DATOS_PERSONALES"]["NOMBRE_COMPLETO"] = $row["nombre_completo"];
-            $arrDatos["DATOS_PERSONALES"]["CIF"] = $row["cif"];
-            $arrDatos["DATOS_PERSONALES"]["PUESTO"] = $row["puesto"];
-            $arrDatos["DATOS_PERSONALES"]["AREA"] = $row["area"];
-            $arrDatos["DATOS_DOMINIO"]["NOMBRE_EQUIPO"] = $row["nombreequipo"];
-            $arrDatos["DATOS_DOMINIO"]["DIRECCION_IP"] = $row["direccionip"];
-            $arrDatos["DATOS_DOMINIO"]["DOMINIO"] = $row["dominio"];
-            $arrDatos["DATOS_DOMINIO"]["USUARIO"] = $row["usuario"];
-            $arrDatos["DATOS_DOMINIO"]["ACTIVO"] = $row["activo"];
+            $arrDatos["INVENTARIO_IT"]["CIF"] = $row["cif"];
+            $arrDatos["INVENTARIO_IT"]["NOMBRE_COMPLETO"] = $row["nombre_completo"];
+            $arrDatos["INVENTARIO_IT"]["PUESTO"] = $row["puesto"];
+            $arrDatos["INVENTARIO_IT"]["AREA"] = $row["area"];
+            $arrDatos["INVENTARIO_IT"]["ACTIVO"] = $row["activo"];
+            $arrDatos["INVENTARIO_IT"]["ACTIVE_DIRECTORY"]["NOMBRE_EQUIPO"] = $row["nombreequipo"];
+            $arrDatos["INVENTARIO_IT"]["ACTIVE_DIRECTORY"]["DIRECCION_IP"] = $row["direccionip"];
+            $arrDatos["INVENTARIO_IT"]["ACTIVE_DIRECTORY"]["DOMINIO"] = $row["dominio"];
+            $arrDatos["INVENTARIO_IT"]["ACTIVE_DIRECTORY"]["USUARIO"] = $row["usuario"];
+            $arrDatos["INVENTARIO_IT"]["HARDWARE"] = obtenerHardware($row["expediente"]);
+            $arrDatos["INVENTARIO_IT"]["SOFTWARE"] = obtenerSoftware($row["expediente"]);
+            $arrDatos["INVENTARIO_IT"]["PLATAFORMAS"] = obtenerPlataformas($row["expediente"]);
         }
     }
 
--- wsinventarioit.php
+++ wsinventarioit.php
@@ -1,5 +1,4 @@
 <?php 
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
@@ -10,25 +9,32 @@ date_default_timezone_set("America/Guatemala");
 include("model/wsinventarioitmodel.php");
 $data = json_decode(file_get_contents('php://input'), true);
 
-if( isset($data['id']) ){
-    $id = trim($data['id']);
-    if( $id != ''){
-        $arrDatos = obtenerData($id);
-        echo json_encode($arrDatos, true);
+if( isset($data['cif']) ){
+    $cif = trim($data['cif']);
+    if( $cif != ''){
+        $arrDatos = obtenerData($cif);
+        if( count($arrDatos)>0 ){
+            echo json_encode($arrDatos, true);
+        }else{
+            header('Content-Type: application/json');
+            header("HTTP/1.1 500 Internal Server Error");
+            $arrJson["respuesta"] = "No se encontraron datos con el cif ingresado.";
+            $jsonSalida = json_encode($arrJson,true);
+            echo json_encode($arrJson, true);
+        }
+        
     }else{
         header('Content-Type: application/json');
         header("HTTP/1.1 500 Internal Server Error");
-        $arrJson["respuesta"] = "El campo id se ingreso vacio.";
+        $arrJson["respuesta"] = "El campo cif se ingreso vacio.";
         $jsonSalida = json_encode($arrJson,true);
         echo json_encode($arrJson, true);
     }
 }else{
     header('Content-Type: application/json');
     header("HTTP/1.1 500 Internal Server Error");
-    $arrJson["respuesta"] = "Debe ingresar el campo id en el json de entrada.";
+    $arrJson["respuesta"] = "Debe ingresar el campo cif en el json de entrada.";
     $jsonSalida = json_encode($arrJson, true);
     echo json_encode($arrJson, true);
 }
-
-
 ?>
